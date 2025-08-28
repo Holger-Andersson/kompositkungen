0c98c3646b9799d6c05c5489a4ab4d4d4e2b1710
Holger-Andersson <hogga89@gmail.com>
2025-08-28 14:26:16 +0200

    Added HTML/css first layout

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,3 @@
 import './style.css'
+
 //import { calculate } from './services/calcService';
-
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-
-// `
-
-//const dropdown = document.getElementById("dropdown") as HTMLSelectElement;
-
-const select = document.createElement("select");
-
-const option1 = document.createElement("option"); 
-option1.value = "855";
-option1.text = "Material1";
-select.appendChild(option1);
-
-const option2 = document.createElement("option");
-option2.value = "858";
-option2.text = "Material2";
-select.appendChild(option2);
-
-document.body.appendChild(select);
