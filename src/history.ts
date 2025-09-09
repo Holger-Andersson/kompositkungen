c96053a7d74832044c08902bc2b80407ef5d1e7a
Holgercode <hogga89@gmail.com>
2025-09-09 14:53:20 +0200

    Added a Historypage, switch between main/history with a button works oneway.

---
+++ src/history.ts
@@ -0,0 +1,42 @@
+import '../style.css';
+// import { renderHome } from '../src/main.ts';
+
+export function renderHistory() {
+document.querySelector('#app')!.innerHTML = `
+<div class="container">
+    <h1>KOMPOSITKUNGEN</h1>
+        <h2>Historik</h2>
+        <button id="switchBack" type="submit">Tillbaka</button>
+        <form id="displayProject" class="section" action="#" method="#"
+    
+        <div class="row">
+
+        <label for="projectHistory">Ange projektnummer</label>
+
+        <input type="number" id="projecthistory" placeholder="tex 18922"/>
+        <button type="submit" id="getProjectHistory">Hämta</button>
+
+</div>
+
+<section class="history-section">
+    <div class="projectCard" id="projectData"></div>
+
+    <div class="label" id="displayProjectnumber></div>
+
+    <div class="label" id=""display></div>
+
+    <div class="label"></div>
+
+    <div class="label"></div>
+
+</section>
+
+</div>
+
+`;
+};
+
+// const switchButton = document.getElementById("switchBack") as HTMLButtonElement;
+// switchButton.addEventListener('click', () => {
+//   renderHome();
+// });
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import '../style.css';
 import { Mats } from './domain/materials';
 import { calculateMix } from './services/calcmix.ts'
+import { renderHistory } from '../src/history.ts'
 
-
+// export function renderHome() {
 document.querySelector('#app')!.innerHTML = `
     <div class="container">
       <h1>KOMPOSITKUNGEN</h1>
 
+      <button id="switch" type="">Historik</button>
       <form id="mix-form" class="section" action="#" method="#">
         <div class="row">
 
@@ -73,6 +75,13 @@ document.querySelector('#app')!.innerHTML = `
       </section>
       </div>
     `;
+// };
+// Button som kör renderpage för att visa historiksida.
+
+const switchButton = document.getElementById("switch") as HTMLButtonElement;
+switchButton.addEventListener('click', () => {
+  renderHistory();
+});
 
 // funktion som hämtar material från material.ts till roll-listan.
 export const selectElement = document.getElementById("material") as HTMLSelectElement;
@@ -85,7 +94,6 @@ Mats.forEach(mat => {
 });
 
 const submitButton = document.getElementById("calculate") as HTMLButtonElement;
-
 submitButton.addEventListener('click', async (event) => {
   event.preventDefault();
 
@@ -119,9 +127,6 @@ submitButton.addEventListener('click', async (event) => {
   });
   console.log(result);
 });
-
-
-
 
 //-----------------------------------------------
 
