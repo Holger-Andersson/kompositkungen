8216185a336196f40213460edb770d6152bcaed6
Holger-Andersson <hogga89@gmail.com>
2025-09-12 12:11:56 +0200

    feat: commented out a not finished function

--- src/services/getpotlife.ts
+++ src/services/getpotlife.ts
@@ -2,15 +2,15 @@
 // jämför med Material tempvärden, exakt matchning? -> return and print pottime minutes.
 // Om inte, Ta de två närmaste värdena och skapa en linjär utveckling av dessa två pottimes minutes och skapa ett värde utifrån input.
 //returna nya pottime minutes och printa det.
-import { getSelectedMaterial } from './getmaterials.ts';
+// import { getSelectedMaterial } from './getmaterials.ts';
 
-export function getPotlifeMinutes() {
-    const mat = getSelectedMaterial();
-    const tempInput = document.getElementById("temperature") as HTMLInputElement;
+// export function getPotlifeMinutes() {
+//     const mat = getSelectedMaterial();
+//     const tempInput = document.getElementById("temperature") as HTMLInputElement;
 
-    if (tempInput >= mat.potlife[10]) {
+//     if (tempInput = mat.potlife[10]) {
         
-    };
+//     };
 
 
-}
+// }
