3dd28fc7b73a6cd8d4917eade9f407698d3e8c96
Holgercode <hogga89@gmail.com>
2025-09-16 14:22:46 +0200

    feat: Vite and express running from same port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-import express from 'express';
+import express from "express";
 import cors from 'cors';
 import { MongoClient } from "mongodb";
+import { fileURLToPath } from "url";
+import path from "path";
+
 
 const url = "mongodb://127.0.0.1:27017"
 const client = new MongoClient(url);
@@ -8,14 +11,15 @@ await client.connect();
 const db = client.db("test");
 const dummy = db.collection("dummy");
 
+const isProduction = process.env.NODE_ENV === "production";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const app = express();
-const port = 1337;
+const port = 3000;
 app.use(express.json());
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
 
 app.post('/form', async (req, res) => {
   try {
@@ -45,6 +49,24 @@ app.get('/dummy/:projectNumber', async (req, res) => {
 
 });
 
+let vite
+if (!isProduction) {
+  const rootHtml = path.join(__dirname, "index.html")
+  const {createServer: createViteServer} = await import ("vite")
+  vite = await createViteServer ({
+    server: {middlewareMode: "html"},
+    appType: "spa",
+    rootHtml
+  })
+  app.use(vite.middlewares)
+} else {
+  const distPath = path.join(__dirname, "dist")
+  app.use(express.static(distPath))
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"))
+  })
+}
+
 app.listen(port, () => {
-  console.log(`Server is LIVE at http://localhost:${port}`);
-});
+       console.log(`running ${isProduction ? "PROD" : "DEV"} server at http://localhost:${port}`)
+   })
