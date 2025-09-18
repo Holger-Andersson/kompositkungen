b4d79d856165a6237d5a257e42c467c56adf1f29
Holger-Andersson <hogga89@gmail.com>
2025-09-18 14:33:43 +0200

    style: fixed whitespacing

--- src/services/savemix.ts
+++ src/services/savemix.ts
@@ -1,25 +1,24 @@
-    export async function saveData() {
+export async function saveData() {
 
-        let project = {
-            projectNumber: Number((document.getElementById("project") as HTMLInputElement).value),
-            material: (document.getElementById("material") as HTMLSelectElement).value,
-            temperature: Number((document.getElementById("temperature") as HTMLInputElement).value),
-            comment: (document.getElementById("comment") as HTMLInputElement).value,
-            partA: (document.getElementById("amountA") as HTMLInputElement).value,
-            partB: (document.getElementById("amountB") as HTMLInputElement).value,
-            partC: (document.getElementById("amountC") as HTMLInputElement).value,
-        }
-        console.log(project);
-        console.log("då");
+  let project = {
+    projectNumber: Number((document.getElementById("project") as HTMLInputElement).value),
+    material: (document.getElementById("material") as HTMLSelectElement).value,
+    temperature: Number((document.getElementById("temperature") as HTMLInputElement).value),
+    comment: (document.getElementById("comment") as HTMLInputElement).value,
+    partA: (document.getElementById("amountA") as HTMLInputElement).value,
+    partB: (document.getElementById("amountB") as HTMLInputElement).value,
+    partC: (document.getElementById("amountC") as HTMLInputElement).value,
+  }
+  console.log(project);
+  console.log("då");
 
-    const result = await fetch("http://localhost:1337/form", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(project)
+  const result = await fetch("http://localhost:3000/form", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(project)
 
-    });
-    console.log(result);
-  };
-
+  });
+  console.log(result);
+};
