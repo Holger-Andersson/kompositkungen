b22a876e8febcf4fdde1a3e8dcf3e0be115d0892
Holger-Andersson <hogga89@gmail.com>
2025-10-13 10:09:38 +0200

    feat: Updated package.json scripts, testing i vitest runs playwright files without excludes like .spec

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vitest/config';
+// import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-  test: {
-    include: ['**/*.test.{js,ts,jsx,tsx}'],
-    exclude: ['node_modules/**', 'dist/**', 'build/**', 'playwright-report/**', 'test-results/**', 'e2e/**', 'tests-examples/**'],
-    environment: 'node'
-  }
-});
+// export default defineConfig({
+//   test: {
+//     include: ['**/*.test.{js,ts,jsx,tsx}'],
+//     exclude: ['node_modules/**', 'dist/**', 'build/**', 'playwright-report/**', 'test-results/**', 'e2e/**', 'tests-examples/**'],
+//     environment: 'node'
+//   }
+// });
